bbc70b2294d6f1d030c25414ba16b09822d330c5
yulia.pryania <yulia.pryania@gmail.com>
2026-09-06 16:55:01 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+}
+
+group = "guru.qa"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
